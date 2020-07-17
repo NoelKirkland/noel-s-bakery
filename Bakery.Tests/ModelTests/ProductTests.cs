d38982f696ecedc5c0300f052e2b3b2dcc2b1805
Noel Kirkland <noelkirkland@gmail.com>
2020-07-17 13:17:09 -0700

    construct two additional tests for spec 5

--- Bakery.Tests/ModelTests/ProductTests.cs
+++ Bakery.Tests/ModelTests/ProductTests.cs
@@ -23,10 +23,24 @@ namespace Bakery.TestTools
     }
     
     [TestMethod]
-    public void PriceCalculator_CalculatesPriceOfBreadBasedOnQuantity_60()
+    public void PriceCalculator_CalculatesPriceOfBreadBasedOnQuantityOf10_35()
+    {
+      Bread newBread = new Bread(10);
+      Assert.AreEqual("35.00", newBread.PriceCalculatorBread());
+    }
+
+    [TestMethod]
+    public void PriceCalculator_CalculatesPriceOfBreadBasedOnQuantityOf11_40()
+    {
+      Bread newBread = new Bread(11);
+      Assert.AreEqual("40.00", newBread.PriceCalculatorBread());
+    }
+
+    [TestMethod]
+    public void PriceCalculator_CalculatesPriceOfBreadBasedOnQuantityOf12_40()
     {
       Bread newBread = new Bread(12);
-      Assert.AreEqual(40, newBread.PriceCalculatorBread());
+      Assert.AreEqual("40.00", newBread.PriceCalculatorBread());
     }
 
     [TestMethod]
--- Bakery/Models/products.cs
+++ Bakery/Models/products.cs
@@ -11,10 +11,43 @@ namespace Bakery.Models
       Quantity = quantity;
     }
 
-    public int PriceCalculatorBread()
+    public string PriceCalculatorBread()
     {
-      return (Quantity * 5);
+      if (Quantity % 3 == 0)
+      {
+        decimal priceDec =  ((Quantity * 5)*(2/3m));
+        string price = priceDec.ToString("N2");
+        return price;
+      }
+      else if (Quantity % 3 == 1)
+      {
+        decimal priceDec =  (((Quantity - 1) * 5)*(2/3m) + 5);
+        string price = priceDec.ToString("N2");
+        return price;
+      }
+      else if (Quantity % 3 == 2)
+      {
+        decimal priceDec =  (((Quantity - 2) * 5)*(2/3m) + 10);
+        string price = priceDec.ToString("N2");
+        return price;
+      }
+      else
+      {
+        return "I'm sorry, there was a problem with your order";
+      }
     }
+
+    // public decimal PriceCalculatorBread()
+    // {
+    //   if (Quantity % 2 == 0)
+    //   {
+    //     return ((Quantity * 5)((Quantity/3) * 5));
+    //   }
+    //   else
+    //   {
+    //     return (Quantity * 5);
+    //   }
+    // }
   }
   public class Pastry
   {
