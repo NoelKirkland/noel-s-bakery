76f1cdb9dcbfe50710d8b2c5a9e2152f53f7b9c1
Noel Kirkland <noelkirkland@gmail.com>
2020-07-17 08:32:10 -0700

    add boilerplate elements to project, run first successfull build

---
+++ Bakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      // Do stuff here close to the end
+    }
+  }
+}
